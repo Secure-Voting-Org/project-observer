0112db10fc96f9a16e43284e4122feeaec5a8f27
Sandeep-Merugumala <sandeepmerugumala480@gmail.com>
2026-02-15 19:50:00 +0530

    Sync local changes to remote

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
         setIsLoading(true);
 
         try {
-            const response = await fetch(`http://${window.location.hostname}:5000/api/observer/login`, {
+            const response = await fetch('/api/observer/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password, role })
